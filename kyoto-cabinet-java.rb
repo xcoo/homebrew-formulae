0d87c1ec47bfcf0385fae0a9f481c31584848756
Toshiki Takeuchi <take@xcoo.jp>
2015-02-10 16:01:33 +0900

    Add kyoto-cabinet-java

---
+++ kyoto-cabinet-java.rb
@@ -0,0 +1,53 @@
+require 'formula'
+
+class KyotoCabinetJava < Formula
+  homepage 'http://fallabs.com/kyotocabinet/'
+  url 'http://fallabs.com/kyotocabinet/javapkg/kyotocabinet-java-1.24.tar.gz'
+  sha1 '1149b31faf65dd2f2b1761fb0b747ddb55b945f3'
+
+  depends_on :java => "1.6+"
+
+  patch :DATA
+
+  def install
+    java_home = ENV["JAVA_HOME"] = `/usr/libexec/java_home`.chomp
+    ENV.append "CPPFLAGS", "-I#{java_home}/include -I#{java_home}/include/darwin"
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make install"
+  end
+end
+
+
+__END__
+--- a/Makefile.in
++++ b/Makefile.in
+@@ -29,6 +29,7 @@ prefix = @prefix@
+ exec_prefix = @exec_prefix@
+ datarootdir = @datarootdir@
+ LIBDIR = @libdir@
++JARDIR = @exec_prefix@/share/java
+ DESTDIR =
+ 
+ # Building configuration
+@@ -91,7 +92,8 @@ untabify :
+ 
+ install :
+ 	mkdir -p $(DESTDIR)$(LIBDIR)
+-	cp -Rf $(JARFILES) $(DESTDIR)$(LIBDIR)
++	mkdir -p $(DESTDIR)$(JARDIR)
++	cp -Rf $(JARFILES) $(DESTDIR)$(JARDIR)
+ 	cp -Rf $(LIBRARYFILES) $(DESTDIR)$(LIBDIR)
+ 	@printf '\n'
+ 	@printf '#================================================================\n'
+--- a/configure
++++ b/configure
+@@ -2192,7 +2192,7 @@ printf '%s\n' "$MYJAVAHOME"
+ printf 'checking JVMPLATFORM... '
+ if uname | grep Darwin >config.tmp
+ then
+-  JVMPLATFORM="mac"
++  JVMPLATFORM="darwin"
+ else
+   for file in `\ls $MYJAVAHOME/include`
+   do

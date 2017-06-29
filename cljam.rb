ea7349134d1247dd8c6789e2f40db7f9e1c1a944
Toshiki Takeuchi <take@xcoo.jp>
2017-06-29 15:59:35 +0900

    cljam 0.4.0

--- cljam.rb
+++ cljam.rb
@@ -1,8 +1,8 @@
 class Cljam < Formula
   desc "Tools for manipulating DNA Sequence Alignment/Map (SAM)"
   homepage "https://chrovis.github.io/cljam/"
-  url "https://github.com/chrovis/cljam/releases/download/0.3.1/cljam", :using => :nounzip
-  sha256 "01861e4af3d267c4e699a9a88029d2a2c5294db8beabf19feeba9b4fc62a73a8"
+  url "https://github.com/chrovis/cljam/releases/download/0.4.0/cljam", :using => :nounzip
+  sha256 "b166defa5f2244d80004fbe3195a8077b2294f292773c94e80495736cdc9aa4e"
 
   depends_on :java
 

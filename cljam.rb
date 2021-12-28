72bfa6fde96b85eaa8bf3074d445843fbf933273
alumi <alumi@users.noreply.github.com>
2021-12-28 12:54:45 +0900

    Depend on openjdk instead of java

--- cljam.rb
+++ cljam.rb
@@ -4,7 +4,7 @@ class Cljam < Formula
   url "https://github.com/chrovis/cljam/releases/download/0.8.3/cljam", :using => :nounzip
   sha256 "7bedabc234106cac532a52fe8fee722049312e17a7b62e6248b156eacc6d4712"
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     chmod 0755, "cljam"

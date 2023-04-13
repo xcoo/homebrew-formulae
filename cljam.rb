fc37bcf438e4e9392c2125b4088c466649256072
alumi <alumi@users.noreply.github.com>
2023-04-13 16:04:26 +0900

    Update cljam to 0.8.4

--- cljam.rb
+++ cljam.rb
@@ -1,8 +1,8 @@
 class Cljam < Formula
   desc "Tools for manipulating DNA Sequence Alignment/Map (SAM)"
   homepage "https://chrovis.github.io/cljam/"
-  url "https://github.com/chrovis/cljam/releases/download/0.8.3/cljam", :using => :nounzip
-  sha256 "7bedabc234106cac532a52fe8fee722049312e17a7b62e6248b156eacc6d4712"
+  url "https://github.com/chrovis/cljam/releases/download/0.8.4/cljam", :using => :nounzip
+  sha256 "9d18a7f5d2fa4e93aa54c0b6663739ca142cee86d2992b3e792bc55a062b9a52"
 
   depends_on "openjdk"
 
